9ef463f9173c83049ad20163102b396f695b43ab
adityamurali155 <adityamurali2@gmail.com>
2024-01-24 19:54:22 +0530

    edited bucket name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "tfga_bucket" {
-  bucket = "terraform-GA-bucket-123"
+  bucket = "terraform-github-actions-bucket-123"
   tags = {
     Name        = "Env"
     Environment = "Prod"
